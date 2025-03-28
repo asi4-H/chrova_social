be06303b663c9ea0db4bb140867474f5ea6cefc2
Asia H. <asiaharrington2002@gmail.com>
2025-03-27 21:56:35 -0400

    Add files via upload

---
+++ models/user.cs
@@ -0,0 +1,27 @@
+﻿namespace ChrovaAPI
+{
+    public class user
+    {
+        private static Random _r = new Random();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+
+        public user(int id, string name, string username)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+        }
+
+        public static List<user> GetUsers() => new List<user>
+        {
+            new user(001, "Aleena West", "TrendyWest"),
+            new user(002, "Josef Wilkins", "Wilkatron"),
+            new user(003, "Nora McNeil", "NorAster"),
+            new user(004, "Hugo Friedman", "InkyHugo"),
+            new user(005, "Imani Shepherd", "ShepAsaur")
+        };
+    }
+}
